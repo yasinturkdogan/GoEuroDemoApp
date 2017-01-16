ab6840bd62533eb6d3a74bf6a5f572b12b5431d2
Yasin Turkdogan <yasinturkdogan@gmail.com>
2017-01-16 23:46:39 +0300

    Add pods

---
+++ Pods/Target Support Files/YTKeychainManager/YTKeychainManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YTKeychainManager : NSObject
+@end
+@implementation PodsDummy_YTKeychainManager
+@end
---
+++ Pods/Target Support Files/YTKeychainManager/YTKeychainManager-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "YTKeychainManager.h"
+
+FOUNDATION_EXPORT double YTKeychainManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char YTKeychainManagerVersionString[];
+
